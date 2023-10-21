e5a296a210d5773f4768c5f3ea4e6a2b31106639
Steffan <steffan.riemann@gmail.com>
2023-10-21 06:51:44 +0200

    able to detect surrounding cells

--- src/maze_generator.py
+++ src/maze_generator.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+class Direction:
+    def __init__(self, direction_coords, coords_to_check, coords_to_color):
+        self.direction_coords = direction_coords,
+        self.coords_to_check = coords_to_check,
+        self.coords_to_color = coords_to_color
+
+    def validate_direction(self, board):
+        rows, cols = board.shape  # Assuming 'board' is a NumPy array
+        for x, y in self.coords_to_check:
+            if x < 0 or y < 0 or x >= rows or y >= cols:
+                return False  # Out of bounds
+            if board[x, y] != 0:
+                return False  # Not a 0 in the board
+        return True  # All checks passed
+
 def display_maze(matrix):
     plt.imshow(matrix.T, cmap='gray_r', extent=[0, matrix.shape[0], 0, matrix.shape[1]], aspect='equal')
     plt.xticks(np.arange(0, matrix.shape[0]+1, 1))
@@ -41,15 +56,48 @@ def create_maze(x, y):
     random_point = valid_points[random.randint(0, len(valid_points) - 1)]
     x, y = random_point
 
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
     valid_directions = []
 
-    for x, y in directions:
+    for direction in directions:
 
+        new_x = random_point[0] + direction[0]
+        new_y = random_point[1] + direction[1]
 
-    return valid_directions
+        value = board[new_x, new_y]
+        if value == 0:
+            valid_directions.append(direction)
+        print(value)
 
+    direction_objs = []
+    for direction in valid_directions:
+        cells_to_check = []
+        cells_to_color = []
+        if direction[0] == 0:
+            for i in range(-2, 2):
+                cells_to_check.append([i, direction[1]])
+                cells_to_check.append([i, direction[1]*2])
+                cells_to_color.append([0 if i == 0 else int(i / abs(i)), direction[1]])
+
+        else:
+            for i in range(-2, 2):
+                cells_to_check.append([direction[0], i])
+                cells_to_check.append([i, direction[1] * 2])
+                cells_to_color.append([direction[0], 0 if i == 0 else int(i / abs(i))])
+
+        direction_coords = [a + b for a, b in zip(direction, random_point)]
+        coords_to_check = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in cells_to_check]
+        coords_to_color = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in cells_to_color]
+
+        direction_objs.append(Direction(direction_coords=direction_coords,
+                                        coords_to_check=coords_to_check,
+                                        coords_to_color=coords_to_color))
+
+        for index, obj in direction_objs:
+            if not obj.validate_direction(board):
+                del direction_objs[index]
     return board
+
 
 def create_checkered_board(x, y):
     board = np.zeros((x, y), dtype=int)
