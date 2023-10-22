ff65d4311b99bcbe94bfaddb4c54309084fbfc31
Steffan <steffan.riemann@gmail.com>
2023-10-22 07:19:13 +0200

    displaying maze while generating

--- src/maze_generator.py
+++ src/maze_generator.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 
 
@@ -26,75 +27,37 @@ class Direction:
         return True  # All checks passed
 
 def display_maze(matrix):
-    plt.imshow(matrix.T, cmap='gray_r', extent=[0, matrix.shape[0], 0, matrix.shape[1]], aspect='equal')
-    plt.xticks(np.arange(0, matrix.shape[0]+1, 1))
-    plt.yticks(np.arange(0, matrix.shape[1]+1, 1))
+    # display once initial line is made. update when:
+    # - all valid points are found, mark them green
+    # - chosen point is marked red, rest back to black
+    # - once all cells are checked, mark valid green, invalid red.
+    # - if all valid, expand maze in all black. else back to choosing another point. keep invalid point red.
+
+    # color scheme:
+    # 0: white
+    # 1: black
+    # 2: blue
+    # 3: green
+    # 4: red
+
+    colors = [(1, 1, 1), (0, 0, 0), (0, 0, 1), (0, 1, 0), (1, 0, 0)]
+    cmap = mcolors.ListedColormap(colors)
+    plt.clf()
+    plt.imshow(matrix.T, cmap=cmap, vmin=0, vmax=4, extent=[0, matrix.shape[0], 0, matrix.shape[1]], aspect='equal')
+    plt.xticks(np.arange(0, matrix.shape[0] + 1, 1))
+    plt.yticks(np.arange(0, matrix.shape[1] + 1, 1))
     plt.grid(True, which='both', color='black', linewidth=0.5)
-    plt.show()
-
-
-def extend_maze(board):
-
-
-    # after this, do dead end filling. basically the same as before. pick random point, try extending a dead-end
-    # when no valid points remain, maze is done.
+    plt.draw()
+    plt.pause(1)
 
 
 
-    # Add all 1's to an array of "valid_points"
-    valid_points = np.argwhere(board == 1)
-
-    # Pick one random point from "valid_points"
-    random_point = valid_points[random.randint(0, len(valid_points) - 1)]
-
-
-    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    valid_directions = []
-
-    for direction in directions:
-
-        new_x = random_point[0] + direction[0]
-        new_y = random_point[1] + direction[1]
-
-        if 0 <= new_x < board.shape[0] and 0 <= new_y < board.shape[1]:
-            value = board[new_x, new_y]
-            if value == 0:
-                valid_directions.append(direction)
-            print(value)
-
+def extend_maze(board, random_point, valid_directions):
     direction_objs = []
     for direction in valid_directions:
-        path_coords = []
-        cells_to_check = []
-        cells_to_color = []
-        if direction[0] == 0:
-            for i in range(-2, 3):
-                path_coords.append([0 if i == 0 else int(i / abs(i)), direction[1]-1])
-                cells_to_check.append([i, direction[1]])
-                cells_to_check.append([i, direction[1]+1])
-                cells_to_color.append([0 if i == 0 else int(i / abs(i)), direction[1]])
+        coords_to_check, coords_to_color, direction_coords, path_coords = investigate_direction(direction,
+                                                                                                random_point)
 
-        else:
-            for i in range(-2, 3):
-                path_coords.append([0 if i == 0 else int(i / abs(i)), direction[1] - 1])
-                cells_to_check.append([direction[0], i])
-                cells_to_check.append([i, direction[1]+2])
-                cells_to_color.append([direction[0], 0 if i == 0 else int(i / abs(i))])
-
-        path_coords = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in path_coords]
-        direction_coords = [a + b for a, b in zip(direction, random_point)]
-        coords_to_check = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in cells_to_check]
-        coords_to_color = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in cells_to_color]
-
-        direction_objs.append(Direction(direction_coords=direction_coords,
-                                        coords_to_check=coords_to_check,
-                                        coords_to_color=coords_to_color,
-                                        path_coords=path_coords))
-
-
-        for i in range(len(direction_objs)):
-            if not direction_objs[i].validate_direction(board):
-                del direction_objs[i]
 
     if len(direction_objs):
         random_direction = random.randint(0, len(direction_objs) - 1)
@@ -107,24 +70,118 @@ def extend_maze(board):
     return board
 
 
-def create_checkered_board(x, y):
-    board = np.zeros((x, y), dtype=int)
-    board[1::2, ::2] = 1
-    board[::2, 1::2] = 1
+def investigate_direction(direction, random_point):
+    path_coords = []
+    cells_to_check = []
+    cells_to_color = []
+    if direction[0] == 0:
+        for i in range(-2, 3):
+            path_coords.append([0 if i == 0 else int(i / abs(i)), direction[1] - 1])
+            cells_to_check.append([i, direction[1]])
+            cells_to_check.append([i, direction[1] * 2])
+            cells_to_color.append([0 if i == 0 else int(i / abs(i)), direction[1]])
+
+    else:
+        for i in range(-2, 3):
+            path_coords.append([0 if i == 0 else int(i / abs(i)), direction[1] - 1])
+            cells_to_check.append([direction[0], i])
+            cells_to_check.append([i, direction[1] * 2])
+            cells_to_color.append([direction[0], 0 if i == 0 else int(i / abs(i))])
+    path_coords = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in path_coords]
+    direction_coords = [a + b for a, b in zip(direction, random_point)]
+    coords_to_check = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in cells_to_check]
+    coords_to_color = [[a + b for a, b in zip(inner_list, random_point)] for inner_list in cells_to_color]
+    return coords_to_check, coords_to_color, direction_coords, path_coords
+
+
+def get_valid_directions(board, random_point):
+    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    valid_directions = []
+    for direction in directions:
+
+        new_x = random_point[0] + direction[0]
+        new_y = random_point[1] + direction[1]
+
+        if 0 <= new_x < board.shape[0] and 0 <= new_y < board.shape[1]:
+            value = board[new_x, new_y]
+            if value == 0:
+                valid_directions.append(direction)
+                board[new_x, new_y] = 3
+    return board, valid_directions
+
+
+def choose_point(board):
+    # change chosen point to value 2
+    valid_points = np.argwhere(board == 1)
+    random_point = valid_points[random.randint(0, len(valid_points) - 1)]
+    board[random_point[0], random_point[1]] = 2
+    return board, random_point
+
+
+def board_setup(width, height):
+    board = np.zeros((width, height), dtype=int)
+    middle_col = height // 2
+    board[:, middle_col] = 1
     return board
 
-# Sample 2D array to test the function
-checkered_maze = create_checkered_board(25, 15)
 
-width = 25
-height = 15
+def fill_board(board, coords, value):
+    rows, cols = board.shape
+    for x, y in coords:
+        if 0 <= x < rows and 0 <= y < cols:
+            board[x, y] = value
 
-board = np.zeros((width, height), dtype=int)
-middle_col = height // 2
-board[:, middle_col] = 1
+def validate_direction(board, coords_to_check):
+    rows, cols = board.shape  # Assuming 'board' is a NumPy array
+    for x, y in coords_to_check:
+        if x < 0 or y < 0 or x >= rows or y >= cols:
+            return False  # Out of bounds
+        if board[x, y] != 0:
+            return False  # Not a 0 in the board
 
-for i in range(100):
-    board = extend_maze(board)
-# display_maze(checkered_maze)
-display_maze(board)
-print("done")
+def validate_path(path_coords):
+    rows, cols = board.shape
+    for x, y in path_coords:
+        if x < 0 or y < 0 or x >= rows or y >= cols:
+            return False  # Out of bounds
+        if board[x, y] != 1:
+            return False  # Not a 1 in the path
+    return True  # All checks passed
+
+if __name__ == "__main__":
+    width = 25
+    height = 15
+    board = board_setup(width, height)
+    plt.ion()
+    display_maze(board)
+
+    for i in range(10):
+        board, random_point = choose_point(board)
+        display_maze(board)
+        board, valid_directions = get_valid_directions(board, random_point)
+        display_maze(board)
+        for direction in valid_directions.copy():
+            coords_to_check, coords_to_color, direction_coords, path_coords = investigate_direction(direction, random_point)
+            if not validate_path(path_coords):
+                print("path not valid!")
+                break
+            fill_board(board, coords_to_check, 3)
+            display_maze(board)
+            if not validate_direction(board, coords_to_check):
+                print("direction not valid!")
+                valid_directions.remove(direction)
+                continue
+        print("whattup")
+        if len(valid_directions):
+            random_direction = random.randint(0, len(valid_directions) - 1)
+        else:
+            break
+        fill_board(board, coords_to_color, 1)
+        display_maze(board)
+        board[board != 1] = 0
+
+        # board = extend_maze(board, random_point, valid_directions)
+        # board[board != 1] = 0
+        display_maze(board)
+
+    plt.ioff()  # Turn off interactive mode
